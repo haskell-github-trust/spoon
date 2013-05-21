a92e0327fcc37324532a127b866c5b15b37a10cb
Liyang HU <git@liyang.hu>
2013-05-21 11:26:57 +0900

    Add Handles to Spoon: allow the use of {tea,}spoonWithHandles

--- Control/Spoon.hs
+++ Control/Spoon.hs
@@ -17,25 +17,48 @@
 -----------------------------------------------------------------------------
 
 
-module Control.Spoon (spoon, teaspoon) where
+module Control.Spoon
+    ( Handles
+    , defaultHandles
+    , spoon
+    , spoonWithHandles
+    , teaspoon
+    , teaspoonWithHandles
+    ) where
 
 import Control.Exception
 import Control.DeepSeq
 import System.IO.Unsafe
 
-handlers :: [Handler (Maybe a)]
-handlers = [ Handler $ \(_ :: ArithException)   -> return Nothing
-           , Handler $ \(_ :: ArrayException)   -> return Nothing
-           , Handler $ \(_ :: ErrorCall)        -> return Nothing
-           , Handler $ \(_ :: PatternMatchFail) -> return Nothing
-           , Handler $ \(x :: SomeException)    -> throwIO x
-           ]
+type Handles a = [Handler (Maybe a)]
+
+{-# INLINEABLE defaultHandles #-}
+defaultHandles :: Handles a
+defaultHandles =
+    [ Handler $ \(_ :: ArithException)   -> return Nothing
+    , Handler $ \(_ :: ArrayException)   -> return Nothing
+    , Handler $ \(_ :: ErrorCall)        -> return Nothing
+    , Handler $ \(_ :: PatternMatchFail) -> return Nothing
+    , Handler $ \(x :: SomeException)    -> throwIO x ]
 
 -- | Evaluate a value to normal form and return Nothing if any exceptions are thrown during evaluation. For any error-free value, @spoon = Just@.
+{-# INLINEABLE spoonWithHandles #-}
+spoonWithHandles :: NFData a => Handles a -> a -> Maybe a
+spoonWithHandles handles a = unsafePerformIO $
+    deepseq a (Just `fmap` return a) `catches` handles
+
+-- | Evaluate a value to normal form and return Nothing if any exceptions are thrown during evaluation. For any error-free value, @spoon = Just@.
+{-# INLINE spoon #-}
 spoon :: NFData a => a -> Maybe a
-spoon a = unsafePerformIO $ deepseq a (Just `fmap` return a) `catches` handlers
+spoon = spoonWithHandles defaultHandles
+
+{-# INLINEABLE teaspoonWithHandles #-}
+teaspoonWithHandles :: Handles a -> a -> Maybe a
+teaspoonWithHandles handles a = unsafePerformIO $
+    (Just `fmap` evaluate a) `catches` handles
 
 -- | Like 'spoon', but only evaluates to WHNF.
+{-# INLINE teaspoon #-}
 teaspoon :: a -> Maybe a
-teaspoon a = unsafePerformIO $ (Just `fmap` evaluate a) `catches` handlers
+teaspoon = teaspoonWithHandles defaultHandles
 
